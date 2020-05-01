9f580262f076d5b04957ad5193e3f38e87b2b8d1
Joe <jwana2@yahoo.com>
2020-05-01 20:38:06 +0700

    remote db

--- database.js
+++ database.js
@@ -1,21 +1,21 @@
 const mysql = require('mysql');
 
 // Create connection
-const pool = mysql.createPool({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'parallel',
-    database : 'chat'
-});
-
-// amazon - internal
 // const pool = mysql.createPool({
-//     host     : process.env.RDS_HOSTNAME,
-//     user     : process.env.RDS_USERNAME,
-//     password : process.env.RDS_PASSWORD,
-//     port     : process.env.RDS_PORT,
+//     host     : 'localhost',
+//     user     : 'root',
+//     password : 'parallel',
 //     database : 'chat'
 // });
+
+// amazon - internal
+const pool = mysql.createPool({
+    host     : process.env.RDS_HOSTNAME,
+    user     : process.env.RDS_USERNAME,
+    password : process.env.RDS_PASSWORD,
+    port     : process.env.RDS_PORT,
+    database : 'chat'
+});
 
 
 // Connect
