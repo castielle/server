65666de3ed365c8d1c8fd9e8cdfe26cc010b73ec
Joe <jwana2@yahoo.com>
2020-05-01 16:28:13 +0700

    rds

--- database.js
+++ database.js
@@ -1,11 +1,19 @@
 const mysql = require('mysql');
 
 // Create connection
+// const pool = mysql.createPool({
+//     host     : 'localhost',
+//     user     : 'root',
+//     password : 'parallel',
+//     database : 'chat'
+// });
+
+// amazon
 const pool = mysql.createPool({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'parallel',
-    database : 'chat'
+    host     : 'rds.custipjdjfst.ap-southeast-1.rds.amazonaws.com',
+    user     : 'admin',
+    password : 'Ig7FNxW8pzpSKNXiNla5',
+    database : 'rds'
 });
 
 // Connect
